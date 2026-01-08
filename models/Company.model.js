3ae1f54f881d33a648a72b8ea520263a66f0d0fa
Shishirthakur2416 <samadkhan040204@gmail.com>
2026-01-08 17:01:10 +0530

    add some fields

--- models/Company.model.js
+++ models/Company.model.js
@@ -34,7 +34,41 @@ const CompanySchema = new mongoose.Schema(
       enum: ["New", "Contacted", "Responded", "Shortlisted"],
       default: "New",
     },
-
+    
+    // Additional fields
+    personName: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    
+    phoneNumber: {
+      type: String,
+      required: false,
+      match: [/^\+?[1-9]\d{1,14}$/, 'Please enter a valid phone number'],
+    },
+    
+    alternatePhoneNumber: {
+      type: String,
+      required: false,
+      match: [/^\+?[1-9]\d{1,14}$/, 'Please enter a valid alternate phone number'],
+    },
+    
+    gstNumber: {
+      type: String,
+      required: false,
+      uppercase: true,
+      trim: true,
+    },
+    
+    panNumber: {
+      type: String,
+      required: false,
+      uppercase: true,
+      trim: true,
+      match: [/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/, 'Please enter a valid PAN number'],
+    },
+    
     // Company document field
     uploadDocument: { type: String }, // URL to company document file in S3
   },
